33561db6ba80cad4cbc140a996f1843089624a86
Toshiki Iga <igapyon@gmail.com>
2016-08-04 14:28:45 +0900

    save

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
 import fish.payara.micro.BootstrapException;
 import fish.payara.micro.PayaraMicro;
 import fish.payara.micro.PayaraMicroRuntime;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.IOException;
 
 public class Main {
-  public static void main(String[] args) throws BootstrapException, IOException {
-      final File file = new File("myapp.war");
-      if (file.exists() == false) {
-          System.out.println("File not found: " + file.getAbsolutePath());
-      }
-      final PayaraMicroRuntime instance = PayaraMicro.getInstance()
-          .setHttpPort(Integer.parseInt(System.getenv("PORT")))
-          .bootStrap();
-      instance.deploy(file);
-  }
+	public static void main(String[] args) throws BootstrapException, IOException {
+		final File file = new File("myapp.war");
+		if (file.exists() == false) {
+			System.out.println("File not found: " + file.getAbsolutePath());
+		}
+		final PayaraMicroRuntime instance = PayaraMicro.getInstance()
+				.setHttpPort(Integer.parseInt(System.getenv("PORT"))).bootStrap();
+		instance.deploy(file);
+	}
 }
